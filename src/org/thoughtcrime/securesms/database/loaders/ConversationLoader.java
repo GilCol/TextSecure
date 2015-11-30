6fcb00bd9490811a96650766b435b366eb922529
pukki <sebastian.pukki@gmail.com>
2015-11-30 18:21:02 +0100

    Added more logs

--- src/org/thoughtcrime/securesms/database/loaders/ConversationLoader.java
+++ src/org/thoughtcrime/securesms/database/loaders/ConversationLoader.java
@@ -2,6 +2,7 @@ package org.thoughtcrime.securesms.database.loaders;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.util.AbstractCursorLoader;
@@ -17,11 +18,13 @@ public class ConversationLoader extends AbstractCursorLoader {
   }
 
   public boolean hasLimit() {
+    Log.d("ConversationLoader", "hasLimit(), limit: " + limit);
     return limit > 0;
   }
 
   @Override
   public Cursor getCursor() {
+    Log.d("ConversationLoader", "hasLimit(), threadId" + threadId + ", limit: " + limit);
     return DatabaseFactory.getMmsSmsDatabase(context).getConversation(threadId, limit);
   }
 }
